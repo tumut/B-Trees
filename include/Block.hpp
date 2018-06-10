565b4acee8a49f536a319ffe8575669a625987b6
Timóteo <mechamutoh@gmail.com>
2018-06-10 01:52:35 -0400

    Translate Block.hpp

--- include/Block.hpp
+++ include/Block.hpp
@@ -1,32 +1,33 @@
 #ifndef _BLOCK_HPP_INCLUDED_
 #define _BLOCK_HPP_INCLUDED_
 
-//! Definição do tamanho do Bloco.
+//! Block size in bytes.
 /*!
-  O tamanho foi definido como sendo de tamanho 4096 por ser o tamanho do setor
-  físico indicado através do comando `fdisk -l` na máquina do Timóteo, um
-  Linux Mint 18.1 Cinnamon 64-bit.
- */
+	The size was defined as 4096 bytes based on sector storage size of the
+	local machine, obtained through an `fdisk -l` command.
+*/
 #define BLOCK_SIZE 4096
 
-//! Union para leitura e escrita, em bloco, de tipos arbitrários.
+//! Union for reading and writing blocks containing serialized data.
 /*!
-  É essencial que `sizeof(T) <= BLOCK_SIZE`. Caso esta condição seja violada,
-  o tamanho da instância de union será maior do que BLOCK_SIZE e o programa
-  estará sujeito a comportamento não definido.
-  
-  Esta union serve para garantir que, ao escrever um tipo em arquivo, ele ocupe
-  o espaço de exatamente um bloco. Os bytes que restarem além do tamanho do tipo
-  terão valores indefinidos no arquivo. Para realizar a leitura, é prático usar
-  novamente uma variável de Block<T>.
-  
-  T deve ser um tipo POD (Plain Old Data type) para poder ser sequenciado no
-  arquivo, além de unions apenas permitirem membros que satisfaçam tal requerimento.
- */
+	T must be a POD (Plain Old Data type) and will be the type of the
+	serialized data within the block.
+
+	The condition `sizeof(T) <= BLOCK_SIZE` must be satisfied. Otherwise, the
+	stored value will be bigger than the block size and reading and writing
+	will be compromised.
+
+	This union is used to guarantee that written data in the files will always
+	individually occupy exactly one block in size. The remaining unused bytes
+	will have undefined values in the file.
+
+	After using Block<T> instances to write a file, you should use it again
+	when reading the file.
+*/
 template <class T>
 union Block {
-	char padding[BLOCK_SIZE]; //!< Vetor de caracteres usado para garantir que a instância tenha no mínimo BLOCK_SIZE bytes.
-	T var; //!< Membro usado para acesso ao valor empacotado no bloco.
+	char padding[BLOCK_SIZE]; //!< Byte array used to guarantee that the instance will have at least `BLOCK_SIZE` bytes.
+	T var; //!< Member used to access the wrapped value.
 };
 
 #endif // _BLOCK_HPP_INCLUDED_
